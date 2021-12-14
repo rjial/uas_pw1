a7d9e080d825445769c3812f127e7de0d0eb6ccf
DeGrees <darkbludevil@gmail.com>
2021-12-14 12:31:41 +0700

    nambah database

--- database.sql
+++ database.sql
@@ -1,18 +1,8 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     30/11/2021 12:42:31                          */
+/* Created on:     14/12/2021 12:29:45                          */
 /*==============================================================*/
 
-
-/*==============================================================*/
-/* Table: ADMIN                                                 */
-/*==============================================================*/
-create table ADMIN
-(
-   USERNAME             varchar(20) not null  comment '',
-   PASSWORD             varchar(40) not null  comment '',
-   primary key (USERNAME)
-);
 
 /*==============================================================*/
 /* Table: AMBIL_LOMBA                                           */
@@ -22,6 +12,16 @@ create table AMBIL_LOMBA
    ID_PESERTA           int not null  comment '',
    ID_LOMBA             int not null  comment '',
    primary key (ID_PESERTA, ID_LOMBA)
+);
+
+/*==============================================================*/
+/* Table: JENIS_USER                                            */
+/*==============================================================*/
+create table JENIS_USER
+(
+   ID_LEVEL             int not null  comment '',
+   NAMA_LEVEL           varchar(50) not null  comment '',
+   primary key (ID_LEVEL)
 );
 
 /*==============================================================*/
@@ -66,6 +66,18 @@ create table PESERTA
    primary key (ID_PESERTA)
 );
 
+/*==============================================================*/
+/* Table: USER                                                  */
+/*==============================================================*/
+create table USER
+(
+   ID_USER              int not null  comment '',
+   ID_LEVEL             int  comment '',
+   ID_USERNAME          int not null  comment '',
+   PASSWORD             varchar(40) not null  comment '',
+   primary key (ID_USER)
+);
+
 alter table AMBIL_LOMBA add constraint FK_AMBIL_LO_AMBIL_LOM_PESERTA foreign key (ID_PESERTA)
       references PESERTA (ID_PESERTA) on delete restrict on update restrict;
 
@@ -74,4 +86,7 @@ alter table AMBIL_LOMBA add constraint FK_AMBIL_LO_AMBIL_LOM_LOMBA foreign key (
 
 alter table PERGURUAN_TINGGI add constraint FK_PERGURUA_MENGADAKA_LOMBA foreign key (ID_LOMBA)
       references LOMBA (ID_LOMBA) on delete restrict on update restrict;
+
+alter table USER add constraint FK_USER_RELATIONS_JENIS_US foreign key (ID_LEVEL)
+      references JENIS_USER (ID_LEVEL) on delete restrict on update restrict;
 
